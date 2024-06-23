6b5f5e7e728d7bf8cf559194db2c886d20bca61a
John Dave Manuel <jdmanuel242@gmail.com>
2024-06-23 11:13:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Wedding Invitation</title>
+        <title>DavestForVianca - May 18 2025</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
